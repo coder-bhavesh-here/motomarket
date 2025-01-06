1f3a9a388ee5646dfd281b69cae58434d483f2aa
Bhavesh <codewithbhaveshdev@gmail.com>
2025-01-06 16:31:37 +0530

    blog correction

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -28,7 +28,7 @@
                     <a href='/blogs/{{ $blog->id }}' class="flex justify-center">
                         @if ($blog->image)
                             {{-- <img src="{{ asset('storage/' . $blog->image) }}" alt="Blog Image" class="blog-image"> --}}
-                            <img src="http://92.205.108.194/wom-admin/public/storage/".{{ $blog->image }}"
+                            <img src="http://92.205.108.194/wom-admin/public/storage/{{ $blog->image }}"
                                 alt="Blog Image" class="blog-image">
                         @elseif (getYoutubeVideoId($blog->youtube_video_link ?? ''))
                             <div style="width: 40%;">
