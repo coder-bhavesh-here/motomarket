5a58fa28faca8f25839ff9fd9e0ce59cb28e9d90
Bhavesh <codewithbhaveshdev@gmail.com>
2025-01-08 17:22:54 +0530

    title change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adventure Page</title>
+    <title>World On Moto</title>
     @vite('resources/css/app.css') <!-- Make sure you're using Laravel Mix or Vite -->
 </head>
 
