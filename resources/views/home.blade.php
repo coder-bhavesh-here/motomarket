1024ebf87119470e77ec03a082d37c1f70411886
Bhavesh <codewithbhaveshdev@gmail.com>
2025-01-21 12:44:03 +0530

    small change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -946,7 +946,7 @@
                                 @if (Route::has('register'))
                                     <a href="{{ route('register') }}"
                                         class="font-bold rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                                        Sign Up
+                                        Sign up
                                     </a>
                                 @endif
                                 <span class="ml-3">
