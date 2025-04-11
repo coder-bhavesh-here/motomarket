1e42b753cc1dc660d38e50608c846dd7cd40ad41
Bhavesh <179925847+coder-bhavesh-here@users.noreply.github.com>
2025-04-11 16:57:13 +0530

    register changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,36 +19,36 @@
 </head>
 
 <body class="bg-gray-100">
-    <div class="w-full flex flex-col lg:flex-row sm:justify-center sm:items-center sm:h-screen">
+    <div class="w-full flex flex-col wommd:flex-row sm:justify-center sm:items-center">
         <!-- Image Section -->
-        <div class="hidden lg:block lg:w-[65%]">
+        <div class="hidden wommd:block wommd:w-[65%]">
             {{-- A slider of images images/bike1.jpg, images/bike2.jpg, images/bike3.jpg, images/bike4.jpg, images/bike5.jpg, images/bike6.jpg --}}
-            <div id="imageSlider" class="relative h-screen w-full overflow-hidden">
+            <div id="imageSlider" class="relative min-h-screen w-full overflow-hidden">
                 <!-- Slider Images -->
-                <div class="slider-images h-screen w-full">
-                    <img class="h-screen w-full object-cover absolute opacity-0 transition-opacity duration-1000 ease-in-out"
+                <div class="slider-images min-h-screen w-full">
+                    <img class="min-h-screen w-full object-cover absolute opacity-0 transition-opacity duration-1000 ease-in-out"
                         src="{{ asset('images/bike1.jpg') }}" alt="Bike Image 1">
-                    <img class="h-screen w-full object-cover absolute opacity-0 transition-opacity duration-1000 ease-in-out"
+                    <img class="min-h-screen w-full object-cover absolute opacity-0 transition-opacity duration-1000 ease-in-out"
                         src="{{ asset('images/bike2.jpg') }}" alt="Bike Image 2">
-                    <img class="h-screen w-full object-cover absolute opacity-0 transition-opacity duration-1000 ease-in-out"
+                    <img class="min-h-screen w-full object-cover absolute opacity-0 transition-opacity duration-1000 ease-in-out"
                         src="{{ asset('images/bike3.jpg') }}" alt="Bike Image 3">
-                    <img class="h-screen w-full object-cover absolute opacity-0 transition-opacity duration-1000 ease-in-out"
+                    <img class="min-h-screen w-full object-cover absolute opacity-0 transition-opacity duration-1000 ease-in-out"
                         src="{{ asset('images/bike4.jpg') }}" alt="Bike Image 4">
-                    <img class="h-screen w-full object-cover absolute opacity-0 transition-opacity duration-1000 ease-in-out"
+                    <img class="min-h-screen w-full object-cover absolute opacity-0 transition-opacity duration-1000 ease-in-out"
                         src="{{ asset('images/bike5.jpg') }}" alt="Bike Image 5">
-                    <img class="h-screen w-full object-cover absolute opacity-0 transition-opacity duration-1000 ease-in-out"
+                    <img class="min-h-screen w-full object-cover absolute opacity-0 transition-opacity duration-1000 ease-in-out"
                         src="{{ asset('images/bike6.jpg') }}" alt="Bike Image 6">
                 </div>
             </div>
         </div>
 
         <!-- Login Section -->
-        <div class="w-full lg:w-[35%] bg-white shadow-md">
+        <div class="w-full wommd:w-[35%] bg-white shadow-md">
             <div
-                class="flex flex-col justify-center lg:justify-start lg:pt-32 items-center min-h-screen sm:w-full lg:h-full px-6 py-12">
+                class="flex flex-col wommd:justify-start pt-0 short:pt-16 wommd:pt-32 items-center min-h-screen sm:w-full wommd:h-full px-6 py-12">
                 <!-- Logo and Header -->
                 <div class="flex flex-row justify-center items-center mb-6 px-16">
-                    <img src="{{ asset('images/logo-text.png') }}"
+                    <img class="max-w-[250px] womsm:max-w-[300px] wommd:max-w-[350px]" src="{{ asset('images/logo-text.png') }}"
                         alt="World On Moto - Explore the world on a motorbike">
                 </div>
 
@@ -61,7 +61,7 @@
                             <td class="w-[82px]">
                             </td>
                             <td class="w-[283px]">
-                                <h2 class="text-3xl lg:text-2xl font-semibold text-gray-700 mb-5 mt-5 text-center">
+                                <h2 class="text-lg womsm:text-xl wommd:text-2xl font-semibold text-gray-700 mb-5 mt-5 text-center">
                                     Create your account
                                 </h2>
                             </td>
@@ -70,11 +70,11 @@
                         <tr>
                             <td class="w-[82px]">
                                 <label style="text-align: right;" for="name"
-                                    class="block text-2xl lg:text-sm font-medium text-gray-700 mr-2 mt-5">Name</label>
+                                    class="block text-sm wommd:text-base font-medium text-gray-700 mr-2 mt-5">Name</label>
                             </td>
                             <td class="w-[283px]">
                                 <input id="name"
-                                    class="w-full mt-5 {{ is_array($errors->get('name')) && !empty($errors->get('name')) ? 'error-input' : '' }} block text-2xl lg:text-sm w-full mt-1 border rounded-lg shadow-sm"
+                                    class="w-full mt-5 {{ is_array($errors->get('name')) && !empty($errors->get('name')) ? 'error-input' : '' }} block text-sm wommd:text-base w-full mt-1 border rounded-lg shadow-sm"
                                     type="text" name="name" :value="old('name')" autofocus
                                     autocomplete="username" />
                             </td>
@@ -82,11 +82,11 @@
                         <tr>
                             <td class="w-[82px]">
                                 <label style="text-align: right;" for="email"
-                                    class="block text-2xl lg:text-sm font-medium text-gray-700 mr-2 mt-2">Email</label>
+                                    class="block text-sm wommd:text-base font-medium text-gray-700 mr-2 mt-2">Email</label>
                             </td>
                             <td class="w-[283px]">
                                 <input id="email"
-                                    class="w-full mt-2 {{ is_array($errors->get('email')) && !empty($errors->get('email')) ? 'error-input' : '' }} block text-2xl lg:text-sm w-full mt-1 border rounded-lg shadow-sm"
+                                    class="w-full mt-2 {{ is_array($errors->get('email')) && !empty($errors->get('email')) ? 'error-input' : '' }} block text-sm wommd:text-base w-full mt-1 border rounded-lg shadow-sm"
                                     type="email" name="email" :value="old('email')" autofocus
                                     autocomplete="username" />
                             </td>
@@ -94,11 +94,11 @@
                         <tr>
                             <td class="w-[82px]">
                                 <label style="text-align: right;" for="password"
-                                    class="block text-2xl lg:text-sm font-medium text-gray-700 mr-2 mt-2">Password</label>
+                                    class="block text-sm wommd:text-base font-medium text-gray-700 mr-2 mt-2">Password</label>
                             </td>
                             <td class="w-[283px]">
                                 <input id="password"
-                                    class="w-full mt-2 {{ is_array($errors->get('password')) && !empty($errors->get('password')) ? 'error-input' : '' }} block text-2xl lg:text-sm w-full mt-1 border rounded-lg shadow-sm"
+                                    class="w-full mt-2 {{ is_array($errors->get('password')) && !empty($errors->get('password')) ? 'error-input' : '' }} block text-sm wommd:text-base w-full mt-1 border rounded-lg shadow-sm"
                                     type="password" name="password" autocomplete="current-password" />
                             </td>
                             <td class="w-[82px]">
@@ -119,10 +119,10 @@
                         <tr>
                             <td colspan="2" class="w-full">
                                 <div class="text-center mt-5">
-                                    <x-input-error :messages="$errors->get('name')" class="mt-2 text-2xl lg:text-sm" />
-                                    <x-input-error :messages="$errors->get('email')" class="mt-2 text-2xl lg:text-sm" />
-                                    <x-input-error :messages="$errors->get('password')" class="mt-2 text-2xl lg:text-sm" />
-                                    <x-input-error :messages="$errors->get('terms-conditions')" class="mt-2 text-2xl lg:text-sm" />
+                                    <x-input-error :messages="$errors->get('name')" class="mt-2 text-sm wommd:text-base" />
+                                    <x-input-error :messages="$errors->get('email')" class="mt-2 text-sm wommd:text-base" />
+                                    <x-input-error :messages="$errors->get('password')" class="mt-2 text-sm wommd:text-base" />
+                                    <x-input-error :messages="$errors->get('terms-conditions')" class="mt-2 text-sm wommd:text-base" />
                                 </div>
                             </td>
                         </tr>
@@ -132,13 +132,13 @@
                                     <input type="checkbox" id="terms-conditions" name="terms-conditions"
                                         class="w-4 h-4">
                                     <label for="terms-conditions"
-                                        class="ml-2 text-2xl lg:text-sm text-gray-600 font-medium">
+                                        class="ml-2 text-sm wommd:text-base text-gray-600 font-medium">
                                         Accept
-                                        <a href="#" class="text-green font-bold underline">
+                                        <a href="#" class="text-green text-sm wommd:text-base font-bold underline">
                                             Terms and Condition
                                         </a>
                                         &
-                                        <a href="#" class="text-green font-bold underline">
+                                        <a href="#" class="text-green text-sm wommd:text-base font-bold underline">
                                             Privacy Policy
                                         </a>
                                     </label>
@@ -148,7 +148,7 @@
                         <tr>
                             <td class="w-[82px]"></td>
                             <td class="w-[283px]">
-                                <button class="mt-5 w-full primary-button font-bold">
+                                <button style="padding: 12px" class="mt-5 w-full primary-button font-bold">
                                     {{ __('Sign up') }}
                                 </button>
                             </td>
