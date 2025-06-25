7106695bac92cca7708d8755ba19612c9e70cbae
Bhavesh <codewithbhaveshdev@gmail.com>
2025-06-25 15:53:42 +0530

    remove button and loader

--- resources/views/tour-management.blade.php
+++ resources/views/tour-management.blade.php
@@ -129,15 +129,6 @@
             @endif
         @endforeach
     </div>
-    <div class="text-center">
-        <button onclick="loadMoreTours();" class="button-text">Load More
-        </button>
-    </div>
-    <div id="loading" class="text-center hidden mb-10">
-        <div
-            class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-green border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]">
-        </div>
-    </div>
 </main>
 
 <!-- Filter Modal -->
