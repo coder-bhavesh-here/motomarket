d3fc1195544332b52cc7ebbf06c374bce2b81269
Bhavesh <codewithbhaveshdev@gmail.com>
2025-07-07 21:43:52 +0530

    link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,7 +95,7 @@
                                         src="{{ asset('storage/' . $user->profile_picture) }}"
                                         alt="Profile Picture" class="rounded-full">
                                 @else
-                                    @if ($user === null)       
+                                    @if (isset($user) && $user === null)       
                                         <a href="/login"><img src="{{ asset('images/user.png') }}" alt=""></a>
                                     @else
                                         <img src="{{ asset('images/user.png') }}" alt="">
