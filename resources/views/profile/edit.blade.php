caba28b82c9ec7c5cc3c300512625a5ca58c322f
Bhavesh <codewithbhaveshdev@gmail.com>
2025-07-10 17:40:37 +0530

    profiles

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,5 +1,15 @@
 <x-app-layout>
     <style>
+        .btn-orange {
+            min-height: 36px !important;
+            color: white !important;
+            border-radius: 5px !important;
+            border: unset !important;
+            font-weight: 700 !important;
+            padding: 6px 20px !important;
+            font-size: 16px !important;
+        }
+
         .iti__country-container button {
             border: unset !important;
         }
@@ -22,7 +32,7 @@
         <div class="mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8">
                 <div class="w-full">
-                    <form method="POST" action="{{ route('profile.updates') }}" class="mt-6 space-y-6" enctype="multipart/form-data">
+                    <form method="POST" action="{{ route('profile.updates') }}" class="mt-6 space-y-6 w-full wommd:w-3/5" enctype="multipart/form-data">
                         @csrf
                         @method('PATCH')
 
@@ -145,14 +155,10 @@
                             <x-input-error class="mt-2" :messages="$errors->get('riding_images')" />
                         </div>
 
-                        <div class="flex items-center gap-4">
-                            <x-primary-button>{{ __('Save') }}</x-primary-button>
-
-                            @if (session('status') === 'profile-updated')
-                                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
-                                    class="text-sm text-gray-600 dark:text-gray-400">{{ __('Saved.') }}</p>
-                            @endif
-                        </div>
+                        <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                            <button type="submit" class="btn-orange w-full">Save</button>
+                            <a href="/profiles" class="btn-orange w-full text-center">Close</a>
+                        </div>    
                     </form>
                     <script>
                         document.addEventListener('DOMContentLoaded', function() {
