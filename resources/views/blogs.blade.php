4a3c98e37f81688688b8716178bc3265d1a28b1a
Bhavesh <codewithbhaveshdev@gmail.com>
2025-07-11 16:47:01 +0530

    blogs

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -12,38 +12,37 @@
     }
 @endphp
 <main class="mt-6">
-    <div class="brand-name ml-8">
+    <div class="brand-name ml-18">
         <b style="font-size: 24px; color:black; margin: 5% 0;">WoM Blog</b>
     </div>
 
-    @foreach ($blogs as $blog)
-        <div class="blog-info text-center">
-            <div class="blog-details inline-flex">
-                <div class="blog-description ml-3">
-                    <a href='/blogs/{{ $blog->id }}' class="mb-5">
-                        <p class="blog-title text-black text-">{{ $blog->title }}</p>
+    <div class="grid grid-cols-1 p-16 womsm:grid-cols-2 wommd:grid-cols-4 gap-6">
+        @foreach ($blogs as $blog)
+            <div class="rounded-2xl overflow-hidden shadow-md bg-white">
+                <div class="relative">
+                    <a href="/blogs/{{ $blog->id }}">
+                        <img class="w-full h-56 object-cover rounded-t-3xl" src="{{ asset('storage/' . $blog->image) }}" alt="Blog Image">
                     </a>
-                    <a href='/blogs/{{ $blog->id }}' class="flex justify-center">
-                        @if ($blog->image)
-                            <img src="{{ asset('storage/' . $blog->image) }}" alt="Blog Image" class="blog-image">
-                        @elseif (getYoutubeVideoId($blog->youtube_video_link ?? ''))
-                            <div style="width: 40%;">
-                                <iframe width="100%" height="400"
-                                    src="https://www.youtube.com/embed/{{ getYoutubeVideoId($blog->youtube_video_link ?? '') }}"
-                                    frameborder="0" allowfullscreen></iframe>
-                            </div>
-                        @endif
-                    </a>
-                    <a href='/blogs/{{ $blog->id }}' class="flex justify-center my-5">
-                        <p style="width: 60%">
-                            {!! Str::limit(strip_tags($blog->content), 500, '...') !!}
-                        </p>
-                    </a>
+            
+                    <div class="absolute top-2 left-2 bg-[#b5a976] text-white text-sm font-semibold px-2 py-1 rounded-md">
+                        {{ \Carbon\Carbon::parse($blog->created_at)->format('Y') }}
+                    </div>
                 </div>
-            </div>
-        </div>
-        <hr>
-    @endforeach
+            
+                <div class="p-4">
+                    <a href="/blogs/{{ $blog->id }}">
+                        <h3 class="text-lg font-bold text-[#0F172A] mb-2 leading-snug">
+                            {{ Str::upper($blog->title) }}
+                        </h3>
+                    </a>
+            
+                    <p class="text-gray-500 text-sm leading-relaxed">
+                        {!! Str::limit(strip_tags($blog->content), 150, '...') !!}
+                    </p>
+                </div>
+            </div>        
+        @endforeach
+    </div>
 </main>
 
 @include('footer')
