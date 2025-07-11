81ff6877300e54aebae41d17d2b67389911a20a2
Bhavesh <codewithbhaveshdev@gmail.com>
2025-07-11 19:41:40 +0530

    blogs

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -18,29 +18,29 @@
 
     <div class="grid grid-cols-1 p-16 womsm:grid-cols-2 wommd:grid-cols-4 gap-6">
         @foreach ($blogs as $blog)
-            <div class="rounded-2xl overflow-hidden shadow-md bg-white">
-                <div class="relative">
-                    <a href="/blogs/{{ $blog->id }}">
+            <a href="/blogs/{{ $blog->id }}">
+                <div class="rounded-2xl overflow-hidden shadow-md bg-white">
+                    <div class="relative">
                         <img class="w-full h-56 object-cover rounded-t-3xl" src="{{ asset('storage/' . $blog->image) }}" alt="Blog Image">
-                    </a>
-            
-                    <div class="absolute top-2 right-2 bg-[#596a37] text-white text-sm font-semibold px-2 py-1 rounded-md">
-                        {{ \Carbon\Carbon::parse($blog->created_at)->format('Y') }}
+                
+                        <div class="absolute top-2 right-2 bg-[#596a37] text-white text-sm font-semibold px-2 py-1 rounded-md">
+                            {{ \Carbon\Carbon::parse($blog->created_at)->format('Y') }}
+                        </div>
                     </div>
-                </div>
-            
-                <div class="p-4">
-                    <a href="/blogs/{{ $blog->id }}">
-                        <h3 class="text-lg font-bold text-[#0F172A] mb-2 leading-snug">
-                            {{ Str::upper($blog->title) }}
-                        </h3>
-                    </a>
-            
-                    <p class="text-gray-500 text-sm leading-relaxed">
-                        {!! Str::limit(strip_tags($blog->content), 150, '...') !!}
-                    </p>
-                </div>
-            </div>        
+                
+                    <div class="p-4">
+                        <a href="/blogs/{{ $blog->id }}">
+                            <h3 class="text-lg font-bold text-[#0F172A] mb-2 leading-snug">
+                                {{ Str::upper($blog->title) }}
+                            </h3>
+                        </a>
+                
+                        <p class="text-gray-500 text-sm leading-relaxed">
+                            {!! Str::limit(strip_tags($blog->content), 150, '...') !!}
+                        </p>
+                    </div>
+                </div>        
+            </a>
         @endforeach
     </div>
 </main>
