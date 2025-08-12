da60d28aea0e02a51b78e69b4565f098cb2dedff
Bhavesh <codewithbhaveshdev@gmail.com>
2025-08-12 13:42:58 +0530

    Update tour-view.blade.php

--- resources/views/tour-view.blade.php
+++ resources/views/tour-view.blade.php
@@ -176,7 +176,7 @@
                                 <div class="text-xs w-52 womsm:text-sm text-left wommd:text-base text-[#0F172A]">
                                     {{ \Carbon\Carbon::parse($price->date)->format('F d, Y') }}</div>
                                 <div class="text-xs text-end womsm:text-sm wommd:text-base womsm:ml-8 w-32 wommd:ml-16 font-bold text-[#0F172A]">
-                                    {{$price->duration_days." days"}}</div>
+                                    {{($price->duration_days + 1)." days"}}</div>
                                 <div class="text-xs text-end womsm:text-sm wommd:text-base womsm:ml-8 w-32 wommd:ml-16 font-bold text-[#0F172A]">
                                     {{ $symbol ." " . number_format($price->price, 2) }}</div>
                                 @php
