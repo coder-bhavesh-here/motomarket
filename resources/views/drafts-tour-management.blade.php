bb30e0afe1c9768ade6b7f6aeee01209e68a8a2a
Bhavesh <codewithbhaveshdev@gmail.com>
2025-08-14 16:51:19 +0530

    Update drafts-tour-management.blade.php

--- resources/views/drafts-tour-management.blade.php
+++ resources/views/drafts-tour-management.blade.php
@@ -1,9 +1,4 @@
 @include('new-header')
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-</script>
 <style>
     td {
         border: unset;
@@ -36,14 +31,7 @@
 <main class="mt-2 px-6">
     <p class="text-green font-semibold"><u><a href="{{ route('homepage') }}">Home</a></u> > Tour Management</p>
     @if(session('success'))
-        <div class="toast align-items-center text-bg-success border-0 show" role="alert" aria-live="assertive" aria-atomic="true">
-            <div class="d-flex">
-                <div class="toast-body">
-                    {{ session('success') }}
-                </div>
-                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-            </div>
-        </div>
+        <x-alert title="{{ session('success') }}" positive />
     @endif
 
     <script>
