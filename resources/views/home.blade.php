b469df503e4c9095a969e4405e30def30b77d8e0
Bhavesh <codewithbhaveshdev@gmail.com>
2025-08-21 15:03:18 +0530

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -92,7 +92,7 @@
 <div>
     <div class="grid grid-cols-1 wommd:grid-cols-2 mt-5 max-w-7xl mx-5 py-8 gap-8 justify-self-center">
         <div class="bg-[#E2E8F0] p-5 rounded-md">
-            <p class="text-xl womsm:text-2xl font-semibold m-2 text-[#0F172A]">About WorldonMoto.com</p>
+            <a href="/about"><p class="text-xl womsm:text-2xl font-semibold m-2 text-[#0F172A]">About WorldonMoto.com</p></a>
             <p class="text-xs womsm:text-xl font-normal m-2 text-[#0F172A]">Welcome to WorldOnMoto, where motorcycle
                 enthusiasts connect
                 with epic adventures worldwide! Discover thrilling routes, meet passionate riders, and
@@ -100,7 +100,7 @@
             <a href="/about"><button class="secondary-button mt-2">read more</button></a>
         </div>
         <div class="bg-[#E2E8F0] p-5 rounded-md">
-            <p class="text-xl womsm:text-2xl font-semibold m-2 text-[#0F172A]">Our very first post here...</p>
+            <a href="/blogs"><p class="text-xl womsm:text-2xl font-semibold m-2 text-[#0F172A]">Our very first post here...</p></a>
             <p class="text-xs womsm:text-xl font-normal m-2 text-[#0F172A]">Welcome to WorldOnMoto, where motorcycle
                 enthusiasts connect with epic adventures worldwide! Discover thrilling routes, meet
                 passionate riders, and explore unforgettable journeys—all on one platform designed for
@@ -110,7 +110,7 @@
     </div>
     <div
         class="mt-2 mb-10 max-w-7xl mx-5 py-8 justify-self-center justify-items-start wommd:justify-items-center text-start wommd:text-center bg-[#E2E8F0] p-5 rounded-md">
-        <p class="font-semibold m-2 text-xl womsm:text-2xl text-[#0F172A]">Do you organise motorbike tours?</p>
+        <a href="/invite-operators"><p class="font-semibold m-2 text-xl womsm:text-2xl text-[#0F172A]">Do you organise motorbike tours?</p></a>
         <p class="text-xs womsm:text-xl m-2 wommd:py-5 wommd:px-10 text-[#0F172A]">We are inviting motorcycle adventure
             tour
             operators from around the world to join us. Together we can provide to best experience to our
