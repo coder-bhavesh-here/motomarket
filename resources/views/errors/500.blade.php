51a81f83cceafcc5b8479410343926e3abc9e4c8
Bhavesh <codewithbhaveshdev@gmail.com>
2025-09-16 13:18:57 +0530

    Update 500.blade.php

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="container text-center mt-10">
         <div class="text-2xl text-orange font-bold mt-6">ERROR::500</div>
-        <h3 class="text-xl text-orange font-semibold mt-6">Something went wrong</h3>
+        <h3 class="text-xl text-orange font-semibold mt-6">Something went wrong, please try again.</h3>
         <button class="primary-button mt-3" onclick="window.location.href='{{ url()->previous() }}'">
             Go Back
         </button>        
