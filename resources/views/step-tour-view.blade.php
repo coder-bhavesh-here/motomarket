360ac49baaa7aff38c71595893a7e8b687351903
Bhavesh <codewithbhaveshdev@gmail.com>
2025-09-16 10:57:23 +0530

    Update step-tour-view.blade.php

--- resources/views/step-tour-view.blade.php
+++ resources/views/step-tour-view.blade.php
@@ -4,6 +4,10 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
 </script>
+<link href="https://cdn.jsdelivr.net/npm/lightgallery@2.0.0-beta.3/css/lightgallery.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/lightgallery@2.0.0-beta.3/css/lg-zoom.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/lightgallery@2.0.0-beta.3/css/lg-share.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.8.3/lightgallery.min.js"></script>
 @endpush
 @php
     $currency = $tour->user->tour_currency;
