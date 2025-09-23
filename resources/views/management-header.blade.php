9fdf3fbe219903d9d884a511cc2992d320ec5cee
Bhavesh <codewithbhaveshdev@gmail.com>
2025-09-23 11:53:48 +0530

    Update management-header.blade.php

--- resources/views/management-header.blade.php
+++ resources/views/management-header.blade.php
@@ -1130,7 +1130,7 @@
                 <script>
                     // Mobile detect function
                     function isMobile() {
-                        return /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
+                        return (/Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent) || window.innerWidth < 500);
                     }
 
                     if (isMobile()) {
