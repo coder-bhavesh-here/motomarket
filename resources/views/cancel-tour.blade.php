f00852f584ac473442a8f34e8ba4edb6f9e49762
Bhavesh <codewithbhaveshdev@gmail.com>
2025-09-29 09:32:02 +0530

    Update cancel-tour.blade.php

--- resources/views/cancel-tour.blade.php
+++ resources/views/cancel-tour.blade.php
@@ -8,6 +8,7 @@
 
 <div class="sm:px-6 lg:px-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <span class="text-red-700 opacity-75">You are about to cancel the tour.</span>
     <p class="mt-4 mb-2 font-semibold text-[#0F172A] text-lg womsm:text-xl wommd:text-2xl">
         {{ $tour->title }} - {{ $tour->countries }}
     </p>
