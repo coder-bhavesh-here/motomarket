dd5b96a99bed6a8dae14d8ce459c41bbc69af2f7
Bhavesh <codewithbhaveshdev@gmail.com>
2025-09-29 09:40:02 +0530

    Update cancel-tour.blade.php

--- resources/views/cancel-tour.blade.php
+++ resources/views/cancel-tour.blade.php
@@ -12,5 +12,18 @@
         {{ $tour->title }} - {{ $tour->countries }}
     </p>
     <span class="text-red-500 opacity-75 font-bold">You are about to cancel the tour.</span>
+    <span>Choose one option</span>
+    <div class="grid grid-cols-1 wommd:grid-cols-2">
+        <div>
+            <div id="refundAction" class="cancel-options">
+                Cancel & get refund
+            </div>
+        </div>
+        <div>
+            <div id="creditsAction" class="cancel-options">
+                Cancel & get credits
+            </div>
+        </div>
+    </div>
 </div>
 @include('footer')
