c861f22d722d064fb73b96019b8061ff9b18a02f
Bhavesh <codewithbhaveshdev@gmail.com>
2025-10-02 13:32:24 +0530

    Update incompleted.blade.php

--- resources/views/incompleted.blade.php
+++ resources/views/incompleted.blade.php
@@ -26,7 +26,7 @@
                             <div class="flex items-center justify-between mt-2">
                                 <span class="flex items-center">
                                     <span><img style="width: 17px; height: 23px;" src="{{ asset('images') . '/map-pin.png' }}" alt=""></span>
-                                    <span class="ml-1 text-sm font-medium wommd:text-base">{{ $tour->countries }}</span>
+                                    <span class="ml-1 text-sm font-medium wommd:text-base">{{ str_replace(",",", ", $tour->countries) }}</span>
                                 </span>
                                 <span class="text-green font-semibold text-sm wommd:text-base"></span>
                             </div>
