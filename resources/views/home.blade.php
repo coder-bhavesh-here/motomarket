f8531dff3548f191c286cd4669eeed89c6ad0900
Bhavesh <codewithbhaveshdev@gmail.com>
2025-10-07 09:56:25 +0530

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,7 +25,7 @@
                             <img
                                     class="rounded-md aspect-square object-cover h-full w-full"
                                     src="{{ isset($tour->images) && isset($tour->images->image_path) ? asset('storage') . '/' . $tour->images->image_path : 'https://photos.smugmug.com/Galleries/Motorcycles/i-jX3tNwR/0/K2H4fw8P5MqPD8SRLWSrRZm4479d3ZvH8HL773j2D/L/cj.photos-_CJ09043-L.jpg' }}"
-                                    alt="Tour photo"></a>
+                                    alt="Tour photo">
                             <h3 class="text-base font-semibold mt-2 text-green truncated-text">
                                 {{ $tour->title }}</h3>
                         </div>
