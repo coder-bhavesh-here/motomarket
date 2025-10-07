dd2b47d597cb5039cb1f934d5d31dd04d08b031a
Bhavesh <codewithbhaveshdev@gmail.com>
2025-10-07 09:55:27 +0530

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,68 +16,70 @@
         @foreach ($tours as $tour_sr => $tour)
             @if ($tour_sr < 8)
                 <div class="rounded flex flex-col p-3">
-                    <div>
-                        {{-- <div class="bg-gray-300 h-32 rounded mb-4"></div> --}}
-                        @php
-                            $tour->images = $tour->images->sortBy('index')->first();
-                        @endphp
-                        <a class="" href='/tour/{{ $tour->id }}'><img
-                                class="rounded-md aspect-square object-cover h-full w-full"
-                                src="{{ isset($tour->images) && isset($tour->images->image_path) ? asset('storage') . '/' . $tour->images->image_path : 'https://photos.smugmug.com/Galleries/Motorcycles/i-jX3tNwR/0/K2H4fw8P5MqPD8SRLWSrRZm4479d3ZvH8HL773j2D/L/cj.photos-_CJ09043-L.jpg' }}"
-                                alt="Tour photo"></a>
-                        <h3 class="text-base font-semibold mt-2 text-green truncated-text">
-                            {{ $tour->title }}</h3>
-                    </div>
-                    <p class="text-sm text-gray-600 flex items-center align-center">
-                        <span>
-                            <svg class="mr-1" width="25" height="32" viewBox="0 0 25 25" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <g filter="url(#filter0_d_1974_642)">
-                                    <path
-                                        d="M19.518 10.536C19.518 16.536 12.1108 22.536 12.1108 22.536C12.1108 22.536 4.70361 16.536 4.70361 10.536C4.70361 8.41431 5.48401 6.37948 6.87313 4.87919C8.26225 3.3789 10.1463 2.53604 12.1108 2.53604C14.0753 2.53604 15.9594 3.3789 17.3485 4.87919C18.7376 6.37948 19.518 8.41431 19.518 10.536Z"
-                                        fill="#7A7A7A" stroke="#7A7A7A" stroke-linecap="round"
-                                        stroke-linejoin="round" />
-                                    <path
-                                        d="M12.1107 13.536C13.6448 13.536 14.8884 12.1929 14.8884 10.536C14.8884 8.87919 13.6448 7.53604 12.1107 7.53604C10.5766 7.53604 9.33301 8.87919 9.33301 10.536C9.33301 12.1929 10.5766 13.536 12.1107 13.536Z"
-                                        fill="white" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
-                                </g>
-                                <defs>
-                                    <filter id="filter0_d_1974_642" x="-3" y="0.536041" width="30.2217" height="32"
-                                        filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                                        <feColorMatrix in="SourceAlpha" type="matrix"
-                                            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
-                                        <feOffset dy="4" />
-                                        <feGaussianBlur stdDeviation="2" />
-                                        <feComposite in2="hardAlpha" operator="out" />
-                                        <feColorMatrix type="matrix"
-                                            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.01 0" />
-                                        <feBlend mode="normal" in2="BackgroundImageFix"
-                                            result="effect1_dropShadow_1974_642" />
-                                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1974_642"
-                                            result="shape" />
-                                    </filter>
-                                </defs>
-                            </svg>
-                        </span>
-                        <span class="flex w-full justify-between">
-                            <span class="text-sm font-semibold text-gray-600">
-                                {{ str_replace(',', ', ', $tour->countries) }}
+                    <a class="" href='/tour/{{ $tour->id }}'>
+                        <div>
+                            {{-- <div class="bg-gray-300 h-32 rounded mb-4"></div> --}}
+                            @php
+                                $tour->images = $tour->images->sortBy('index')->first();
+                            @endphp
+                            <img
+                                    class="rounded-md aspect-square object-cover h-full w-full"
+                                    src="{{ isset($tour->images) && isset($tour->images->image_path) ? asset('storage') . '/' . $tour->images->image_path : 'https://photos.smugmug.com/Galleries/Motorcycles/i-jX3tNwR/0/K2H4fw8P5MqPD8SRLWSrRZm4479d3ZvH8HL773j2D/L/cj.photos-_CJ09043-L.jpg' }}"
+                                    alt="Tour photo"></a>
+                            <h3 class="text-base font-semibold mt-2 text-green truncated-text">
+                                {{ $tour->title }}</h3>
+                        </div>
+                        <p class="text-sm text-gray-600 flex items-center align-center">
+                            <span>
+                                <svg class="mr-1" width="25" height="32" viewBox="0 0 25 25" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <g filter="url(#filter0_d_1974_642)">
+                                        <path
+                                            d="M19.518 10.536C19.518 16.536 12.1108 22.536 12.1108 22.536C12.1108 22.536 4.70361 16.536 4.70361 10.536C4.70361 8.41431 5.48401 6.37948 6.87313 4.87919C8.26225 3.3789 10.1463 2.53604 12.1108 2.53604C14.0753 2.53604 15.9594 3.3789 17.3485 4.87919C18.7376 6.37948 19.518 8.41431 19.518 10.536Z"
+                                            fill="#7A7A7A" stroke="#7A7A7A" stroke-linecap="round"
+                                            stroke-linejoin="round" />
+                                        <path
+                                            d="M12.1107 13.536C13.6448 13.536 14.8884 12.1929 14.8884 10.536C14.8884 8.87919 13.6448 7.53604 12.1107 7.53604C10.5766 7.53604 9.33301 8.87919 9.33301 10.536C9.33301 12.1929 10.5766 13.536 12.1107 13.536Z"
+                                            fill="white" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
+                                    </g>
+                                    <defs>
+                                        <filter id="filter0_d_1974_642" x="-3" y="0.536041" width="30.2217" height="32"
+                                            filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                            <feColorMatrix in="SourceAlpha" type="matrix"
+                                                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                            <feOffset dy="4" />
+                                            <feGaussianBlur stdDeviation="2" />
+                                            <feComposite in2="hardAlpha" operator="out" />
+                                            <feColorMatrix type="matrix"
+                                                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.01 0" />
+                                            <feBlend mode="normal" in2="BackgroundImageFix"
+                                                result="effect1_dropShadow_1974_642" />
+                                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1974_642"
+                                                result="shape" />
+                                        </filter>
+                                    </defs>
+                                </svg>
                             </span>
-                            <span class="text-sm font-semibold text-green">
-                                @php
-                                    $currency = $tour->user->tour_currency;
-                                    $symbol = match ($currency) {
-                                        'euro' => '€',
-                                        'usd' => '$',
-                                        'gbp' => '£',
-                                        default => '€',
-                                    };
-                                @endphp
-                                {{ isset($tour->prices[0]) ? $symbol . ' ' . number_format($tour->prices[0]->price, 2) : 'N/A' }}
+                            <span class="flex w-full justify-between">
+                                <span class="text-sm font-semibold text-gray-600">
+                                    {{ str_replace(',', ', ', $tour->countries) }}
+                                </span>
+                                <span class="text-sm font-semibold text-green">
+                                    @php
+                                        $currency = $tour->user->tour_currency;
+                                        $symbol = match ($currency) {
+                                            'euro' => '€',
+                                            'usd' => '$',
+                                            'gbp' => '£',
+                                            default => '€',
+                                        };
+                                    @endphp
+                                    {{ isset($tour->prices[0]) ? $symbol . ' ' . number_format($tour->prices[0]->price, 2) : 'N/A' }}
+                                </span>
                             </span>
-                        </span>
-                    </p>
+                        </p>
+                    </a>
                 </div>
             @else
             @endif
