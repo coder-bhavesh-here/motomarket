90bba42c5f00ba4c2f0b4628268dbe18c6ea4661
Bhavesh <codewithbhaveshdev@gmail.com>
2025-10-22 12:14:18 +0530

    Update show.blade.php

--- resources/views/tour_operators/show.blade.php
+++ resources/views/tour_operators/show.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="py-12">
+    <div class="py-6">
         <div class="mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8">
                 @if ($user !== null)
