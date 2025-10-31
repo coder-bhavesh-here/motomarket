d25822b52f0d48b7243b0a21e70b0510635a60ba
Bhavesh <codewithbhaveshdev@gmail.com>
2025-10-31 10:24:55 +0530

    Update change-password.blade.php

--- resources/views/profile/change-password.blade.php
+++ resources/views/profile/change-password.blade.php
@@ -23,24 +23,23 @@
             <p class="text-green font-semibold"><u><a href="{{ route('homepage') }}">Home</a></u> > <u><a href="{{ route('profiles') }}">Settings</a></u> > Your Details</p>
             <span class="block text-green text-xl womsm:text-2xl wommd:text-3xl font-bold my-6">Your Details</span>
             <div class="w-full">
-                <form method="POST" action="{{ route('profile.updates') }}" class="mt-6 space-y-6 w-full wommd:w-3/5" enctype="multipart/form-data">
+                <form method="POST" action="{{ route('password.change.request') }}" class="mt-6 space-y-6 w-full wommd:w-3/5">
                     @csrf
-                    @method('PATCH')
                     <div>
                         <label class="font-bold text-black" for="name">Current password</label>
-                        <x-text-input type="password" id="current_password" name="current_password" type="text" class="mt-1 block w-full" :value="old('current_password')"
+                        <x-text-input type="password" id="current_password" name="current_password" class="mt-1 block w-full" :value="old('current_password')"
                             required autofocus autocomplete="current_password" />
                         <x-input-error class="mt-2" :messages="$errors->get('current_password')" />
                     </div>
                     <div>
                         <label class="font-bold text-black" for="last_name">New Password</label>
-                        <x-text-input type="password" id="new_password" name="new_password" type="text" class="mt-1 block w-full" :value="old('new_password')"
+                        <x-text-input type="password" id="new_password" name="new_password" class="mt-1 block w-full" :value="old('new_password')"
                             required autofocus autocomplete="name" />
                         <x-input-error class="mt-2" :messages="$errors->get('new_password')" />
                     </div>
                     <div>
                         <label class="font-bold text-black" for="last_name">Confirm Password</label>
-                        <x-text-input type="password" id="new_password_confirmation" name="new_password_confirmation" type="text" class="mt-1 block w-full" :value="old('new_password_confirmation')"
+                        <x-text-input type="password" id="new_password_confirmation" name="new_password_confirmation" class="mt-1 block w-full" :value="old('new_password_confirmation')"
                             required autofocus autocomplete="name" />
                         <x-input-error class="mt-2" :messages="$errors->get('new_password_confirmation')" />
                     </div>
